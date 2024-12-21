8ad0fc6b5a79817674845f1c9cb4762eee528aad
Tamim Al Hasan <talhasan06@gmail.com>
2024-12-21 15:25:21 +0600

    dist update

--- dist/app/modules/admin/admin.route.js
+++ dist/app/modules/admin/admin.route.js
@@ -3,6 +3,6 @@ import auth from "../../middlewares/auth.js";
 import { USER_ROLE } from "../user/user.constant.js";
 import { AdminControllers } from "./admin.controller.js";
 const router = express.Router();
-router.post("/users/:userId/block", auth(USER_ROLE.admin), AdminControllers.blockUser);
+router.patch("/users/:userId/block", auth(USER_ROLE.admin), AdminControllers.blockUser);
 router.delete("/blogs/:id", auth(USER_ROLE.admin), AdminControllers.deleteBlog);
 export const AdminRoutes = router;
--- dist/app/modules/blog/blog.route.js
+++ dist/app/modules/blog/blog.route.js
@@ -6,7 +6,7 @@ import { BlogValidations } from "./blog.validation.js";
 import validateRequest from "../../middlewares/validateRequest.js";
 const router = express.Router();
 //will call controller function
-router.post("/", auth(USER_ROLE.admin, USER_ROLE.user), validateRequest(BlogValidations.createValidationSchema), BlogControllers.createBlog);
+router.post("/", auth(USER_ROLE.user), validateRequest(BlogValidations.createValidationSchema), BlogControllers.createBlog);
 // router.get("/:id", BlogControllers.getSingleBlog);
 router.patch("/:id", auth(USER_ROLE.user), validateRequest(BlogValidations.updateValidationSchema), BlogControllers.updateBlog);
 router.get("/", BlogControllers.getAllBlog);
